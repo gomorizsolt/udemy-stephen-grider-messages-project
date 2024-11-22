ea46b08547a92658439caf1678385d5737004021
gomorizsolt <zsgomoridev@icloud.com>
2024-11-22 09:45:14 +0100

    feat: add placeholder route handlers

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  findAll() {
+    // TODO ...
+  }
+
+  @Get('/:id')
+  findOne() {
+    // TODO ...
+  }
+
+  @Post()
+  create() {
+    // TODO ...
+  }
+
+  @Delete('/:id')
+  delete() {
+    // TODO ...
+  }
+}
